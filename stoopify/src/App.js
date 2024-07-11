8868d4eddaca1bbedd2e875f9c219fcb1c47d328
vaishnavi-3969 <vaishnavi.kale3011@gmail.com>
2024-07-12 02:28:40 +0530

    routes added and images added

--- stoopify/src/App.js
+++ stoopify/src/App.js
@@ -1,11 +1,40 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Landing from './pages/Landing';
+import Contact from './components/Contact';
+import StoopSale from './pages/StoopSale';
+import Register from './pages/Register';
+import Profile from './pages/Profile';
+import Error from './pages/Error';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import { useAuth0 } from '@auth0/auth0-react';
+import Home from './pages/Home';
+import StoopSaleDetail from './pages/StoopSaleDetail';
 
 function App() {
+  const {isAuthenticated} = useAuth0();
+
+  const routes = [
+    { path: '/', element: isAuthenticated ? <Home /> : <Landing /> },
+    { path: '/contact', element: <Contact /> },
+    { path: '/directory', element: <StoopSale /> },
+    { path: '/stoop-sale-register', element: <Register /> },
+    { path: '/profile', element: <Profile /> },
+    { path: '*', element: <Error /> },
+    { path: '/stoopSale/:id', element: <StoopSaleDetail /> },
+  ]
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <div class="bg-[#f1f2eb] ">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          {routes.map(route => (
+            <Route path={route.path} element={route.element} exact />
+          ))}
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ stoopify/src/assets/index.js
@@ -0,0 +1,13 @@
+import Image1 from './image1.jpg';
+import Image2 from './image2.jpeg';
+import Image3 from './image3.jpg';
+import Image4 from './image4.jpg';
+import Image5 from './image5.jpeg';
+
+export {
+    Image1,
+    Image2,
+    Image3,
+    Image4,
+    Image5
+}
