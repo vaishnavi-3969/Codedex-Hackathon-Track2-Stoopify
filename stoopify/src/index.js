5f155db52f9b002049ccd6f585c01b0ff0233dee
vaishnavi-3969 <vaishnavi.kale3011@gmail.com>
2024-07-12 02:35:43 +0530

    auth0 added

--- stoopify/src/index.js
+++ stoopify/src/index.js
@@ -2,12 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-mw55ey2uqk3bkl0r.us.auth0.com"
+      clientId="00cNohaddUyyWR6xQGzVOGlqqFt0JWMs"
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
-
-
